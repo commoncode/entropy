f15eedc98528b4123c3f29bba9b0adb969163908
Adam Hoddinott <adam@messymind.net>
2013-04-05 10:35:48 +1100

    Bumping version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup( name='entropy',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Base classes framework for modern Django projects',
     author = 'Daryl Antony',
     author_email = 'daryl@commoncode.com.au',
