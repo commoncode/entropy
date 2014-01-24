806dbfe8117d0212f198fd6274edc7791c2e91bc
Daryl Antony <daryl@commoncode.com.au>
2014-01-24 23:25:14 +1100

    rename to django-entropy

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup( name='entropy',
-    version = '0.0.3',
+setup( name='django-entropy',
+    version = '0.0.3', # Jump this to 9 to avoid conflict with the other entropy package on pypi
     description = 'Base classes framework for modern Django projects',
     author = 'Daryl Antony',
     author_email = 'daryl@commoncode.com.au',
